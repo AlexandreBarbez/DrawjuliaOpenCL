4a7a8a8638ebc7a68eee42d8a1e9b22378864d08
Alexandre Barbez <alexandre.barbez@gmail.com>
2015-05-27 12:10:56 +0200

    Initial Commit

---
+++ DrawjuliaOpenCL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DrawjuliaOpenCL
+//
+//  Created by Alx on 27/05/2015.
+//  Copyright (c) 2015 Alx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
